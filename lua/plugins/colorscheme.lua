02cb0656c44e1964819ae7c8c3b429dc32421352
Henry Acosta <henry@mftech.io>
2024-01-11 17:14:41 -0500

    core: tokyonight coloshenma transparent

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,15 +1,15 @@
 return {
   { "catppuccin/nvim", name = "catppuccin", opts = { flavour = "macchiato" } },
-  -- {
-  --   "folke/tokyonight.nvim",
-  --   opts = {
-  --     transparent = true,
-  --     styles = {
-  --       sidebars = "transparent",
-  --       floats = "transparent",
-  --     },
-  --   },
-  -- },
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
   {
     "ellisonleao/gruvbox.nvim",
   },
@@ -17,11 +17,23 @@ return {
     "loctvl842/monokai-pro.nvim",
   },
   {
+    "craftzdog/solarized-osaka.nvim",
+    Lazy = true,
+    priority = 1000,
+    opts = function()
+      return {
+        transparent = true,
+      }
+    end,
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
       -- colorscheme = "monokai-pro-spectrum",
       -- colorscheme = "gruvbox",
-      colorscheme = "tokyonight-night",
+      -- colorscheme = "tokyonight-night",
+      -- colorscheme = "solarized-osaka",
+      colorscheme = "tokyonight",
     },
   },
 }
