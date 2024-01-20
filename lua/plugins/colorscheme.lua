771ae033fe7e5253aae384025eb2184556a4c2eb
Henry Acosta <henry@mftech.io>
2024-01-19 20:03:56 -0500

    core: disabled pluggins

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,38 +1,39 @@
 return {
-  { "catppuccin/nvim", name = "catppuccin", opts = { flavour = "macchiato" } },
-  {
-    "folke/tokyonight.nvim",
-    opts = {
-      transparent = true,
-      styles = {
-        sidebars = "transparent",
-        floats = "transparent",
-      },
-    },
-  },
-  {
-    "ellisonleao/gruvbox.nvim",
-  },
-  {
-    "loctvl842/monokai-pro.nvim",
-  },
-  {
-    "craftzdog/solarized-osaka.nvim",
-    priority = 1000,
-    opts = function()
-      return {
-        transparent = true,
-      }
-    end,
-  },
   -- {
-  --   "LazyVim/LazyVim",
+  --   "folke/tokyonight.nvim",
   --   opts = {
-  --     -- colorscheme = "monokai-pro-spectrum",
-  --     -- colorscheme = "gruvbox",
-  --     -- colorscheme = "tokyonight-night",
-  --     -- colorscheme = "solarized-osaka",
-  --     colorscheme = "tokyonight",
+  --     transparent = true,
+  --     styles = {
+  --       sidebars = "transparent",
+  --       floats = "transparent",
+  --     },
   --   },
   -- },
+  {
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
+    config = true,
+  },
+  -- {
+  --   "loctvl842/monokai-pro.nvim",
+  -- },
+  -- {
+  --   "craftzdog/solarized-osaka.nvim",
+  --   priority = 1000,
+  --   opts = function()
+  --     return {
+  --       transparent = true,
+  --     }
+  --   end,
+  -- },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      -- colorscheme = "monokai-pro-spectrum",
+      colorscheme = "gruvbox",
+      -- colorscheme = "tokyonight-night",
+      -- colorscheme = "solarized-osaka",
+      -- colorscheme = "habamax",
+    },
+  },
 }
---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,12 @@
+return {
+
+  { "folke/tokyonight.vim", enabled = false },
+  { "nvim-treesitter/nvim-treesitter", enabled = false },
+  { "nvim-treesitter/nvim-treesitter-textobjects", enabled = false },
+  { "nvim-treesitter/nvim-treesitter-context", enabled = false },
+  { "windwp/nvim-ts-autotag", enabled = false },
+  { "nvim-treesitter/playground", enabled = false },
+  { "echasnovski/mini.indentscope", enabled = false },
+  { "rcarriga/nvim-notify", enabled = false },
+  { "catppuccin", enabled = false },
+}
